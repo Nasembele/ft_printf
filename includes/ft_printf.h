e6720085d8a1c1c6e9850bbce6e1412a590bd8a5
Nasembele <50178818+Nasembele@users.noreply.github.com>
2020-02-27 13:40:15 +0300

    Add files via upload

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: soyster <soyster@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/30 01:18:14 by soyster           #+#    #+#             */
-/*   Updated: 2020/02/26 22:41:40 by soyster          ###   ########.fr       */
+/*   Updated: 2020/02/27 13:36:07 by soyster          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,15 @@ typedef struct	s_float
 	int			len_wh;
 }				t_float;
 
+void	ft_print_fl(t_float *fl, t_flag *all_mod);
+int		ft_act_width(t_float *fl, t_flag *all_mod);
+int		ft_len_whole(long num_start);
+void	ft_print_sp(t_float *fl, t_flag *all_mod);
+void	ft_print_fl_min(t_float *fl, t_flag *all_mod);
+void 	ft_check_s(t_float *fl, t_flag *all_mod);
+void	ft_fill_frt(t_float *fl, int prc);
+void	ft_separ_fl(t_float *fl, int prc);
+int		ft_float_1(long double f, int prc);
 void	ft_fill_fl(t_float *fl);
 int		ft_rounding(t_float *fl);
 void	ft_spec_f(va_list all_arg, t_flag *all_mod);
--- srcs/ft_main.c
+++ srcs/ft_main.c
@@ -6,30 +6,34 @@
 /*   By: soyster <soyster@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/30 01:18:14 by soyster           #+#    #+#             */
-/*   Updated: 2020/02/26 21:28:32 by soyster          ###   ########.fr       */
+/*   Updated: 2020/02/27 12:58:10 by soyster          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+#include "float.h"
 
 int	main(void)
 {
 	//ft_printf("%-05.0f\n", 7.3); // иногда segfault
-	//printf("%-05.0f", 7.3);
+	//ft_printf("%-05.0f", 7.3);
 	//ft_printf("%.10Lf\n", 0.87650894255l);
 	//ft_printf("%f\n", 145.1465675);
-	ft_printf("%f", 7.434);
+	//ft_printf("%f\n", 7.434);
 	//printf("%.10Lf", 0.87650894255l);
 	//printf("%f", -56.2012685);
 	//printf("%.10Lf\n", 0.87650894255l);
-	//ft_printf("%.9Lf\n", 0.87650894255l);
+	ft_printf("%.9Lf\n", -0.87650894255l);
 	//printf("%.9Lf\n", 0.87650894255l);
 
-	//printf("%.10Lf\n", 0.87650894255l);
+	//t_printf("%.10Lf\n", 0.87650894255l);
 	//ft_printf("%f\n", 0.000001); //иногда segfault
 	//ft_printf("%f\n", 0.999999); //иногда segfault
 	//ft_printf("%f\n", 43.43999949); //иногда segfault
 	//ft_printf("%f\n", -985.765426499); //иногда segfault
 	//printf("%f\n", -985.765426499); //иногда segfault
+	
+	//ft_printf("%f\n", FLT_MAX);
+	//printf("%f\n", DBL_MAX);
 	return (0);
 }
